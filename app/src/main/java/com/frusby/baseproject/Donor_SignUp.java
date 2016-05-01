83deeeca27888e496aa03892b51e3f25fbb395c7
WOMYNxH4CKERS <c.walkr>
2016-04-30 17:06:13 -0700

    submit

--- app/src/main/java/com/frusby/baseproject/Donor_SignUp.java
+++ app/src/main/java/com/frusby/baseproject/Donor_SignUp.java
@@ -1,11 +1,15 @@
 package com.frusby.baseproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class Donor_SignUp extends AppCompatActivity {
 
@@ -15,6 +19,7 @@ public class Donor_SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_donor__sign_up);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -25,5 +30,15 @@ public class Donor_SignUp extends AppCompatActivity {
             }
         });
     }
+
+    @OnClick(R.id.button_submit)
+        public void buttonSubmit() {
+        Intent intent = new Intent(this, MainActivityRefugee.class);
+
+        intent.putExtra("data", "test");
+
+        startActivity(intent);
+    }
+
 
 }
